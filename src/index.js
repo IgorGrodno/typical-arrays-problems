f193bef2857e6df919dbf25b302a3d7cc42465c7
IgorGrodno <5850462@mail.ru>
2021-03-14 12:10:29 +0300

    feat: add all code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    let result = 0;
+    if (array != undefined && array.length > 0) {
+        result = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (result > array[i]) {
+                result = array[i];
+            }
+        }
+    }
+    return result;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let result = 0;
+    if (array != undefined && array.length > 0) {
+        result = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (result < array[i]) {
+                result = array[i];
+            }
+        }
+    }
+    return result;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let result = 0;
+    if (array != undefined && array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            result = result + array[i];
+        }
+        result = result / array.length;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return result;
+};
